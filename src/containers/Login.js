ccfb27d75d20d3ab33e844dc8fc9a2b7f7aa3a25
Guillaume <guillaumegozillon@gmail.com>
2021-06-28 17:33:48 +0200

    Coverage fix login + billUI

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,14 +4,23 @@ export let PREVIOUS_LOCATION = ''
 
 // we use a class so as to test its methods in e2e tests
 export default class Login {
-  constructor({ document, localStorage, onNavigate, PREVIOUS_LOCATION, firestore }) {
+  constructor({ 
+    document, 
+    localStorage, 
+    onNavigate, 
+    PREVIOUS_LOCATION, 
+    firestore 
+  }) {
+
     this.document = document
     this.localStorage = localStorage
     this.onNavigate = onNavigate
     this.PREVIOUS_LOCATION = PREVIOUS_LOCATION
     this.firestore = firestore
+
     const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`)
     formEmployee.addEventListener("submit", this.handleSubmitEmployee)
+
     const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`)
     formAdmin.addEventListener("submit", this.handleSubmitAdmin)
   }
@@ -35,8 +44,9 @@ export default class Login {
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      // Changer 'employee' par 'admin'
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,9 +19,16 @@ const row = (bill) => {
     `)
   }
 
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  const rows = (data) => {
+    const sortedData =
+      data && data.length
+        ? data.sort((a, b) => new Date(b.date) - new Date(a.date))
+        : '';
+  
+    return data && data.length
+      ? sortedData.map((bill) => row(bill)).join('')
+      : '';
+  };
 
 export default ({ data: bills, loading, error }) => {
   
