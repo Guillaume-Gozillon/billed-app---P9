9817b1b548a0c9434c642aa6701b2d5b8bc3d9d5
Guillaume <guillaumegozillon@gmail.com>
2021-07-04 03:08:06 +0200

    Init test bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,13 +1,13 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import firebase from "../__mocks__/firebase"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: []})
       document.body.innerHTML = html
-      //to-do write expect expression
     })
     // Test si les dates sont triées par ordre décroissant
     test("Then bills should be ordered from earliest to latest", () => {
@@ -19,4 +19,30 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+  // test loading page sur BillUI
+  test('Then, Loading page should be rendered', () => {
+    // BillUI.loading, condition qui retourne la loading page si TRUE
+    const html = BillsUI({ loading: true })
+    document.body.innerHTML = html
+    // Attend une valeur retour du DOM, verifie que le contenu est TRUE
+    expect(screen.getAllByText('Loading...')).toBeTruthy()
+    console.log(screen.getAllByText('Loading...'));
+  })
+  // test error page
+  test('Then, Error page should be rendered', () => {
+    const html = BillsUI({ error: 'some error message' })
+    document.body.innerHTML = html
+    expect(screen.getAllByText('Erreur')).toBeTruthy()
+  })
+  // test d'intégration GET
+  test('fetches bills from mock API GET', async () => {
+		const getSpy = jest.spyOn(firebase, 'get')
+		const bills = await firebase.get()
+		expect(getSpy).toHaveBeenCalledTimes(1)
+		expect(bills.data.length).toBe(4)
+	})
 })
+
+  // test click on new bill button
+  // test click on eye button
--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -2,7 +2,6 @@
 import { ROUTES_PATH } from '../constants/routes.js'
 import { formatDate, formatStatus } from "../app/format.js"
 import Logout from "./Logout.js"
-import BillsUI from "../views/BillsUI.js"
 
 export default class {
   constructor({ document, onNavigate, firestore, localStorage }) {
@@ -19,8 +18,6 @@ export default class {
     })
 
     new Logout({ document, localStorage, onNavigate })
-
-    console.log(BillsUI);
   }
 
   handleClickNewBill = e => {
